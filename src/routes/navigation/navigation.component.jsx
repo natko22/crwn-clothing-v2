fda75051fc38c30437490e92e07249cb98f3a7d6
Natassa <112552395+natko22@users.noreply.github.com>
2023-06-08 15:45:39 +0200

    add navigation,install react-dom 18

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,20 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>Logo</div>
+        </Link>
+        <div className="links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Navigation;
