57a25d56ddb4e761b4eb7ee0a35b03f8c44c1caf
Natassa <112552395+natko22@users.noreply.github.com>
2023-06-09 15:16:57 +0200

    create sign-up form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-
+import SignUpForm from "./components/sign-up-form/sign-up-form.component";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import SignIn from "./routes/sign-in/sign-in.component";
